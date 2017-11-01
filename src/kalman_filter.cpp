b382c73daccecdf76019c865b381663daf7330be
satoshi <satoshi.kumano@kii.com>
2017-11-01 20:29:49 +0900

    Implementing EKF.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,9 +1,15 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "kalman_filter.h"
+#include <math.h>
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using namespace std;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -25,6 +31,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +41,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +61,26 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  VectorXd z_pred = VectorXd(3);
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float norm = sqrt(pow(px,2) + pow(py,2));
+  z_pred(0) = norm;
+
+  if (norm == 0) {
+    cout << "Can't update EKF. norm == 0" << endl;
+    return;
+  }
+
+  float rad = atan2(py, px);
+  if (rad < 0) {
+    rad = 2 * M_PI - rad;
+  }
+  z_pred(1) = rad;
+  z_pred(3) = (px * vx + py * vy) / norm;
+
+  VectorXd y = z - z_pred;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -4,6 +4,7 @@
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+using namespace std;
 
 Tools::Tools() {}
 
@@ -22,4 +23,35 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//TODO: YOUR CODE HERE
+	float normSqrt = pow(px, 2) + pow(py, 2);
+	float norm = sqrt(normSqrt);
+
+	//check division by zero
+	if (normSqrt == 0) {
+	    cout << "Can not compute Jacobian." << endl;
+	    return Hj;
+	}
+	//compute the Jacobian matrix
+	Hj(0,0) = px / norm;
+	Hj(0,1) = py / norm;
+	Hj(0,2) = 0;
+	Hj(0,3) = 0;
+	Hj(1,0) = - (py / normSqrt);
+	Hj(1,1) = px / normSqrt;
+	Hj(1,2) = 0;
+	Hj(1,3) = 0;
+	Hj(2,0) = py * (vx*py - vy*px) / pow(norm, 3);
+	Hj(2,1) = px * (vy*px - vx*py) / pow(norm, 3);
+	Hj(2,2) = px / norm;
+	Hj(2,3) = py / norm;
+
+	return Hj;
 }
