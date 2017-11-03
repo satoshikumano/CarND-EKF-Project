c62575654dbca851f13924fda554509c973e62b1
satoshi <satoshi.kumano@kii.com>
2017-11-03 19:00:06 +0900

    Removed unused variables.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -19,8 +19,6 @@ FusionEKF::FusionEKF() {
   // initializing matrices
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
-  H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -42,8 +42,6 @@ private:
   Tools tools;
   Eigen::MatrixXd R_laser_;
   Eigen::MatrixXd R_radar_;
-  Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
 
   float noise_ax_;
 	float noise_ay_;
